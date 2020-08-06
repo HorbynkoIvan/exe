32162e2f529b000a4acfd6645b572bab54b5ffbb
HorbynkoIvan <horbynko.i@gmail.com>
2020-08-06 18:08:08 +0300

    added script to handle situation when user use tab for focus

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log("hi")
+$(document).ready(function () {
+        $('.works_btns button, .works_btns a').on('focus', function (e) {
+            $(this).parents('.works__element').addClass('works__element--active');
+        })
+        $('.works_btns button, .works_btns a').on('blur', function (e) {
+            $(this).parents('.works__element').removeClass('works__element--active');
+        })
+    }
+)
